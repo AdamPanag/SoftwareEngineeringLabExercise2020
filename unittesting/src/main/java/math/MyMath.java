8be6691b5478bda774cc109ce699f440b3286370
AdamPanag <adampanag06@gmail.com>
2020-04-18 03:27:58 +0300

    Add isPrime

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -23,4 +23,24 @@ public class MyMath {
 			throw new IllegalArgumentException("Cannot count the factorial of values below 0 or above 12.");
 		}
 	}
+	
+	/*
+	 * Return true if the argument is a prime number and false
+	 * if it is not. Also, if the given number is below 2, it
+	 * will throw an IllegalArgumentException.
+	 */
+	public boolean isPrime(int n) {
+		if (n >= 2) {
+			int i = 2;
+			while (i < n) {
+				if ((n % i) == 0) {
+					return false;					
+				}
+				i++;
+			}
+		} else {
+			throw new  IllegalArgumentException("The given number must be greater than 1.");
+		}
+		return true;
+	}
 }
