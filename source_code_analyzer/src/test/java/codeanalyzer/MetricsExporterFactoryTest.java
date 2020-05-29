18bd0a1bc742e51c532c5bc9eee496a577b6e216
AdamPanag <adampanag06@gmail.com>
2020-05-29 18:04:04 +0300

    Update MetricsExporterFactoryTest to increase code coverage

--- source_code_analyzer/src/test/java/codeanalyzer/MetricsExporterFactoryTest.java
+++ source_code_analyzer/src/test/java/codeanalyzer/MetricsExporterFactoryTest.java
@@ -1,13 +1,26 @@
 package codeanalyzer;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MetricsExporterFactoryTest {
 	
 	MetricsExporterFactory mef = new MetricsExporterFactory();
 	
+	@Test
+	public void testCreateMetricsExporterWithCsvFileType() {
+		MetricsExporter actual = mef.createMetricsExporter("csv");
+		Assert.assertTrue(actual instanceof CsvWriter);
+	}
+	
+	@Test
+	public void testCreateMetricsExporterWithJsonFileType() {
+		MetricsExporter actual = mef.createMetricsExporter("json");
+		Assert.assertTrue(actual instanceof JsonWriter);
+	}
+	
 	@Test(expected = IllegalArgumentException.class)
-	public void testWriteFileWithUknownFIleType() {
+	public void testCreateMetricsExporterWithUknownFIleType() {
 		mef.createMetricsExporter("non-existing-type");
 	}
 }
